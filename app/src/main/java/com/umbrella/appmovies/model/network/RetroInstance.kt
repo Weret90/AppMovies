b25b184bae7a7744bcfae8b09b6cb07452eba719
Weret90 <apostol-ea@mail.ru>
2021-08-16 03:23:59 +0900

    Исправил замечания и ошибки

--- app/src/main/java/com/umbrella/appmovies/model/network/RetroInstance.kt
+++ app/src/main/java/com/umbrella/appmovies/model/network/RetroInstance.kt
@@ -1,11 +1,13 @@
 package com.umbrella.appmovies.model.network
 
+import com.umbrella.appmovies.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetroInstance {
     companion object {
         private const val baseUrl = "https://api.themoviedb.org/3/discover/"
+        const val apiKey = BuildConfig.TMDB_API_KEY
         fun getRetroInstance(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(baseUrl)
